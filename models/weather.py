fde9232fa601ea45f5e1cbecc87b1b936e636da5
bigdaditor <bigdaditor1015@gmail.com>
2025-11-28 18:50:58 +0900

    refactor - Change library SQLAlchemy to SQLModel

--- models/weather.py
+++ models/weather.py
@@ -1,17 +1,23 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
-from core.db import Base
+from sqlmodel import SQLModel, Field
 
-
-class Weather(Base):
+class Weather(SQLModel, table=True):
     __tablename__ = "weather"
 
-    date = Column(String, nullable=False, primary_key=True)
-    avg_temp = Column(Float, nullable=False)
-    min_temp = Column(Float, nullable=False)
-    max_temp = Column(Float, nullable=False)
-    sum_rain = Column(Float, nullable=False)
-    avg_humidity = Column(Float, nullable=False)
-    summary = Column(String, nullable=True)
-    one_hour_rain = Column(Float, nullable=False)
+    date: str = Field(nullable=False, primary_key=True)
+    avg_temp: float = Field(nullable=False)
+    min_temp: float = Field(nullable=False)
+    max_temp: float = Field(nullable=False)
+    sum_rain: float = Field(nullable=False)
+    avg_humidity: float = Field(nullable=False)
+    one_hour_rain: float = Field(nullable=False)
+    summary: str = Field(nullable=False)
 
+class WeatherCreate(SQLModel):
+    date: str
+    avg_temp: float
+    min_temp: float
+    max_temp: float
+    sum_rain: float
+    avg_humidity: float
+    one_hour_rain: float
 
